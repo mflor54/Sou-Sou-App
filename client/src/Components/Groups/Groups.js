45d61297dc4d5bc5c729e4be5ebcb8f617ffa38c
Rachel91649 <rachelnjackson@yahoo.com>
2018-03-19 22:10:41 -0400

    added list items and react-bootstrap props

--- client/src/Components/Groups/Groups.js
+++ client/src/Components/Groups/Groups.js
@@ -1,1 +1,27 @@
 import React, { Component } from 'react';
+import { ListGroup, ListGroupItem, Grid, Row, Col } from 'react-bootstrap';
+
+
+const Groups = () => {
+  return(
+    <div>
+    <Grid>
+      <ListGroup>
+        <ListGroupItem header="Group 1" href="#">
+          
+        </ListGroupItem>
+        <ListGroupItem header="Group 2" href="#">
+          
+        </ListGroupItem>
+        <ListGroupItem header="Group 3" href="#">
+          
+        </ListGroupItem>
+      </ListGroup>
+    </Grid>
+    </div>
+  )
+
+}
+
+
+export default Groups;
