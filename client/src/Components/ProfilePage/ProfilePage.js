13773310896fd4be76eb8ca36f88ed10f27985a6
KrystalWillock <hire.krys@gmail.com>
2018-03-19 21:48:27 -0400

    profile scaffold

--- client/src/Components/ProfilePage/ProfilePage.js
+++ client/src/Components/ProfilePage/ProfilePage.js
@@ -1,1 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react'
+
+const Profile = ({user}) =>{
+    return(
+        <div>
+            <h1>{user}'s Profile</h1>
+
+            
+            </div>
+    )
+}
+
+export default Profile;
+
+
