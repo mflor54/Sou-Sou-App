a559d57fb90443bc976141114ea65001bae26e8c
Rachel91649 <rachelnjackson@yahoo.com>
2018-03-21 12:18:29 -0400

    starting GroupProfile skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 
 import Landing from './Components/Landing/Landing'
 import ProfilePage from './Components/ProfilePage/ProfilePage'
-import Groups from './Components/Groups/Groups'
+import GroupProfile from './Components/GroupProfile/GroupProfile'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Landing/>
-        <ProfilePage/>
-        <Groups />
+        <GroupProfile />
+        
       </div>
     );
   }
--- client/src/Components/GroupProfile/GroupProfile.js
+++ client/src/Components/GroupProfile/GroupProfile.js
@@ -1,1 +1,38 @@
 import React, { Component } from 'react';
+import { Col, Grid, Row, } from 'react-bootstrap';
+
+
+const GroupProfile = () => {
+  return(
+    <div>
+      navbar
+      <Grid>
+        <Row className="show-grid">
+          <Col md={6}> 
+            <h1>Group Profile page</h1>
+          </Col>
+        </Row>
+        <Row className="show-grid">
+          <Col md={6}>
+            Group Description
+          </Col>
+          <Col md={6}>
+            Group member Icons
+          </Col>
+        </Row>
+        <Row className="show-grid">
+          <Col md={6}>
+            <button>Join/Closed</button>
+          </Col>
+        </Row>
+      </Grid>
+      <div>
+        Chat Box
+      </div>
+      
+    </div>
+  )
+}
+
+
+export default GroupProfile;
