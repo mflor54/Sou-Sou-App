68c1b3811c3de861382fc0e7c324ff680637a3f9
Rachel91649 <rachelnjackson@yahoo.com>
2018-03-20 20:58:43 -0400

    changed 2 images and added more dummy data to 2nd row

--- client/src/Components/Groups/Groups.js
+++ client/src/Components/Groups/Groups.js
@@ -11,26 +11,41 @@ const Groups = () => {
     <div>
     <Grid>
       <ListGroup>
-        <ListGroupItem header="Group 1" href="#">
-        <Row>
-          <Col sm={4}>
-            Savings Amount $$$
-          </Col>
-          <Col sm={4}>
-            Group description (cuts off after x-amount of characters)
-          </Col>
-          <Col sm={4}>
-            <img style={avatarStyle} src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
-            <img style={avatarStyle} src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
-            <img style={avatarStyle} src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
-            <img style={avatarStyle} src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
-            <img style={avatarStyle} src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
-            <img style={avatarStyle} src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
-          </Col>
-        </Row>
+        <ListGroupItem header="Debt Payoff Club" href="#">
+          <Row>
+            <Col sm={4}>
+              <p>Savings Goal: $1000</p>
+            </Col>
+            <Col sm={4}>
+              <p>Group description (cuts off after x-amount of characters)</p>
+            </Col>
+            <Col sm={4}>
+              <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
+              <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
+              <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
+              <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
+              <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
+              <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
+            </Col>
+          </Row>
         </ListGroupItem>
-        <ListGroupItem header="Group 2" href="#">
-          
+        <ListGroupItem header="Rainy Day Fund" href="#">
+        <Row>
+            <Col sm={4}>
+              <p>Savings Goal: $500</p>
+            </Col>
+            <Col sm={4}>
+              <p>Group description (cuts off after x-amount of characters)</p>
+            </Col>
+            <Col sm={4}>
+              <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
+              <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
+              <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
+              <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
+              <img style={avatarStyle} src="https://image.freepik.com/icones-gratis/perfil-simbolo-de-informacao_318-41154.jpg"/>
+              <img style={avatarStyle} src="https://image.freepik.com/icones-gratis/perfil-simbolo-de-informacao_318-41154.jpg"/>
+            </Col>
+          </Row>
         </ListGroupItem>
         <ListGroupItem header="Group 3" href="#">
           
