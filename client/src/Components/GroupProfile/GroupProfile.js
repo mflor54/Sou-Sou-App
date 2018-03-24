689da758c120ee2cb3196ed7e16f36d07e9f83e1
Rachel91649 <rachelnjackson@yahoo.com>
2018-03-24 12:10:30 -0400

    added stuff, don't remember what

--- client/src/Components/GroupProfile/GroupProfile.js
+++ client/src/Components/GroupProfile/GroupProfile.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Jumbotron, Button, Col, Grid, Row, Panel, Glyphicon } from 'react-bootstrap';
+import { Jumbotron, Col, Grid, Row, Panel, Glyphicon } from 'react-bootstrap';
+import { Link, Route, Switch } from 'react-router-dom';
+import { ModalContainer, ModalRoute } from 'react-router-modal';
+import { Button} from 'mdbreact';
 
 import './GroupProfile.css';
 
@@ -8,146 +11,155 @@ const avatarStyle = {
   width: '50px',
   marginRight: '5px'
 }
+
 let disable_toggle = false;
-const GroupProfile = () => {
+class GroupProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showJoin: false
+    }
+  }
   //join button logic depends on: 
     //number of possible group members
     //number of ppl currently in group
     //if group is full status = true
-  return(
-    <div>
-      navbar
+  render() {
+    return(
       <div>
-        <Grid>
-          <Row className="show-grid">
-            <Col md={12}> 
-              <h1 className="gp-title">Get Schmoney Team</h1>
-            </Col>
-          </Row>
-          <Row className="show-grid">
-            <Col md={6}>
-              <p>
-                Group Description: Cat ipsum dolor sit amet, paw at your fat belly annoy the old grumpy cat, start a fight and then retreat to wash when i lose howl on top of tall thing. Loved it, hated it, loved it, hated it. Scratch the box intrigued by the shower.
-              </p>
-            </Col>
-            <Col md={6}>
-              Next Payout Date: April 1, 2018
-              <div className="">
-                <img style={avatarStyle} alt="username" src="https://image.flaticon.com/icons/svg/12/12324.svg"/>
-              </div>
-              <div>
-                <img style={avatarStyle} alt="username" src="https://image.flaticon.com/icons/svg/12/12324.svg"/>
-                <img style={avatarStyle} alt="username" src="https://image.flaticon.com/icons/svg/12/12324.svg"/>
-                <img style={avatarStyle} alt="username" src="https://image.flaticon.com/icons/svg/12/12324.svg"/>
-              </div>
-            </Col>
-          </Row>
-          <Row className="show-grid">
-            <Col md={6}>
-              <Button disabled={disable_toggle}>Join</Button>
-            </Col>
-          </Row>
-        </Grid>
-      </div>
+        navbar
+        <div>
+          <Grid>
+            <Row className="show-grid">
+              <Col md={12}> 
+                <h1 className="gp-title">Get Schmoney Team</h1>
+              </Col>
+            </Row>
+            <Row className="show-grid">
+              <Col md={6}>
+                <p>
+                  Group Description: Cat ipsum dolor sit amet, paw at your fat belly annoy the old grumpy cat, start a fight and then retreat to wash when i lose howl on top of tall thing. Loved it, hated it, loved it, hated it. Scratch the box intrigued by the shower.
+                </p>
+              </Col>
+              <Col md={6}>
+                Next Payout Date: April 1, 2018
+                <div className="">
+                  <img style={avatarStyle} alt="username" src="https://image.flaticon.com/icons/svg/12/12324.svg"/>
+                </div>
+                <div>
+                  <img style={avatarStyle} alt="username" src="https://image.flaticon.com/icons/svg/12/12324.svg"/>
+                  <img style={avatarStyle} alt="username" src="https://image.flaticon.com/icons/svg/12/12324.svg"/>
+                  <img style={avatarStyle} alt="username" src="https://image.flaticon.com/icons/svg/12/12324.svg"/>
+                </div>
+              </Col>
+            </Row>
+            <Row className="show-grid">
+              <Col md={6}>
+                <Button disabled={disable_toggle}>Join</Button>
+              </Col>
+            </Row>
+          </Grid>
+        </div>
 
-      <Panel>
-        <Panel.Heading>
-          Message Board
-        </Panel.Heading>
-        <Panel.Body>
-          <ul className="chat">
-            <li className="left clearfix">
-              <span className="chat-img pull-left">
-                <img className="img-circle" src="http://placehold.it/50/A430A8/fff&text=C" alt="user-avatar"  />
-              </span>
-              <div className="chat-body clearfix">
-                <div className="header">
-                  <strong className="pull-left">Crystal</strong> 
-                  <small className="pull-right">
-                    <span className="glyphicon glyphicon-time"></span>
-                    20min ago
-                  </small>
+        <Panel>
+          <Panel.Heading>
+            Message Board
+          </Panel.Heading>
+          <Panel.Body>
+            <ul className="chat">
+              <li className="left clearfix">
+                <span className="chat-img pull-left">
+                  <img className="img-circle" src="http://placehold.it/50/A430A8/fff&text=C" alt="user-avatar"  />
+                </span>
+                <div className="chat-body clearfix">
+                  <div className="header">
+                    <strong className="pull-left">Crystal</strong> 
+                    <small className="pull-right">
+                      <span className="glyphicon glyphicon-time"></span>
+                      20min ago
+                    </small>
+                  </div>
+                  <p className="chat-message pull-left">Is this going to show up where I want it to?</p>
                 </div>
-                <p className="chat-message pull-left">Is this going to show up where I want it to?</p>
-              </div>
-            </li>
-            <li className="left clearfix">
-              <span className="chat-img pull-left">
-                <img className="img-circle" src="http://placehold.it/50/08CEC7/fff&text=M" alt="user-avatar"  />
-              </span>
-              <div className="chat-body clearfix">
-                <div className="header">
-                  <strong className="pull-left">Mike</strong> 
-                  <small className="pull-right">
-                    <span className="glyphicon glyphicon-time"></span>
-                    20min ago
-                  </small>
+              </li>
+              <li className="left clearfix">
+                <span className="chat-img pull-left">
+                  <img className="img-circle" src="http://placehold.it/50/08CEC7/fff&text=M" alt="user-avatar"  />
+                </span>
+                <div className="chat-body clearfix">
+                  <div className="header">
+                    <strong className="pull-left">Mike</strong> 
+                    <small className="pull-right">
+                      <span className="glyphicon glyphicon-time"></span>
+                      20min ago
+                    </small>
+                  </div>
+                  <p className="chat-message pull-left">Is this going to show up where I want it to?</p>
                 </div>
-                <p className="chat-message pull-left">Is this going to show up where I want it to?</p>
-              </div>
-            </li>
-            <li className="left clearfix">
-              <span className="chat-img pull-left">
-                <img className="img-circle" src="http://placehold.it/50/29D9F4/fff&text=K" alt="user-avatar"  />
-              </span>
-              <div className="chat-body clearfix">
-                <div className="header">
-                  <strong className="pull-left">Krystal</strong> 
-                  <small className="pull-right">
-                    <span className="glyphicon glyphicon-time"></span>
-                    20min ago
-                  </small>
+              </li>
+              <li className="left clearfix">
+                <span className="chat-img pull-left">
+                  <img className="img-circle" src="http://placehold.it/50/29D9F4/fff&text=K" alt="user-avatar"  />
+                </span>
+                <div className="chat-body clearfix">
+                  <div className="header">
+                    <strong className="pull-left">Krystal</strong> 
+                    <small className="pull-right">
+                      <span className="glyphicon glyphicon-time"></span>
+                      20min ago
+                    </small>
+                  </div>
+                  <p className="chat-message pull-left">Is this going to show up where I want it to? What happens if I put a shit ton of text in here because I'm angry! Arrrrarrrrrrrrrrrhahshshghghshshghg</p>
                 </div>
-                <p className="chat-message pull-left">Is this going to show up where I want it to? What happens if I put a shit ton of text in here because I'm angry! Arrrrarrrrrrrrrrrhahshshghghshshghg</p>
-              </div>
-            </li>
-            <li className="left clearfix">
-              <span className="chat-img pull-left">
-                <img className="img-circle" src="http://placehold.it/50/25283D/fff&text=R" alt="user-avatar"  />
-              </span>
-              <div className="chat-body clearfix">
-                <div className="header">
-                  <strong className="pull-left">Rachel</strong> 
-                  <small className="pull-right">
-                    <span className="glyphicon glyphicon-time"></span>
-                    20min ago
-                  </small>
+              </li>
+              <li className="left clearfix">
+                <span className="chat-img pull-left">
+                  <img className="img-circle" src="http://placehold.it/50/25283D/fff&text=R" alt="user-avatar"  />
+                </span>
+                <div className="chat-body clearfix">
+                  <div className="header">
+                    <strong className="pull-left">Rachel</strong> 
+                    <small className="pull-right">
+                      <span className="glyphicon glyphicon-time"></span>
+                      20min ago
+                    </small>
+                  </div>
+                  <p className="chat-message pull-left">Is this going to show up where I want it to?</p>
                 </div>
-                <p className="chat-message pull-left">Is this going to show up where I want it to?</p>
-              </div>
-            </li>
-            <li className="left clearfix">
-              <span className="chat-img pull-left">
-                <img className="img-circle" src="http://placehold.it/50/29D9F4/fff&text=K" alt="user-avatar"  />
-              </span>
-              <div className="chat-body clearfix">
-                <div className="header">
-                  <strong className="pull-left">Krystal</strong> 
-                  <small className="pull-right">
-                    <span className="glyphicon glyphicon-time"></span>
-                    20min ago
-                  </small>
+              </li>
+              <li className="left clearfix">
+                <span className="chat-img pull-left">
+                  <img className="img-circle" src="http://placehold.it/50/29D9F4/fff&text=K" alt="user-avatar"  />
+                </span>
+                <div className="chat-body clearfix">
+                  <div className="header">
+                    <strong className="pull-left">Krystal</strong> 
+                    <small className="pull-right">
+                      <span className="glyphicon glyphicon-time"></span>
+                      20min ago
+                    </small>
+                  </div>
+                  <p className="chat-message pull-left">Is this going to show up where I want it to?</p>
                 </div>
-                <p className="chat-message pull-left">Is this going to show up where I want it to?</p>
-              </div>
-            </li>
-          </ul>
-        </Panel.Body>
+              </li>
+            </ul>
+          </Panel.Body>
 
-        <Panel.Footer>
-          <div className="input-group">
-            <input type="text" className="form-control" placeholder="type your message here...."/>
-            <span className="input-group-btn">
-              <button className="btn btn-secondary" type="button">Send</button>
-            </span>
-          </div>
-        </Panel.Footer>
-        
-      </Panel>
+          <Panel.Footer>
+            <div className="input-group">
+              <input type="text" className="form-control" placeholder="type your message here...."/>
+              <span className="input-group-btn">
+                <button className="btn btn-secondary" type="button">Send</button>
+              </span>
+            </div>
+          </Panel.Footer>
           
-      
-    </div>
-  )
+        </Panel>
+            
+        
+      </div>
+    )
+  }
 }
 
 
