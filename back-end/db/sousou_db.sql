f4677979f46a7f69248cc72adf4987ea87a5dc16
Michael Flor <mflor@Michaels-MacBook-Air.local>
2018-03-25 17:52:31 -0400

    added db file for all

--- back-end/db/sousou_db.sql
+++ back-end/db/sousou_db.sql
@@ -1,6 +1,8 @@
 DROP DATABASE IF EXISTS sousou;
 CREATE DATABASE sousou;
 
+\c sousou
+
 CREATE TABLE users (
     ID SERIAL PRIMARY KEY,
     email VARCHAR UNIQUE,
@@ -12,7 +14,7 @@ CREATE TABLE users (
     amount INTEGER,
     rating VARCHAR,
     stripe_id VARCHAR UNIQUE,
-    image BYTEA DEFAULT('https://png.icons8.com/windows/1600/owl.png'),
+    image_ BYTEA DEFAULT('https://png.icons8.com/windows/1600/owl.png'),
     username VARCHAR UNIQUE
 );
 
@@ -20,25 +22,29 @@ CREATE TABLE groups (
     ID SERIAL PRIMARY KEY,
     total_members INTEGER,
     creator VARCHAR,
+    pay_in_amount NUMERIC (6, 2),
+    pay_out_amount NUMERIC (6, 2),
     total_amount INTEGER,
     date_created TIMESTAMPTZ,
     frequency VARCHAR,
     payout VARCHAR,
     group_name VARCHAR UNIQUE,
     rating VARCHAR,
-    description VARCHAR
+    description_ VARCHAR
 );
 
 CREATE TABLE paymentsIn (
     ID SERIAL PRIMARY KEY,
     payment_id VARCHAR UNIQUE,
     amount INTEGER,
-    user_id VARCHAR REFERENCES users(id)
+    user_id VARCHAR REFERENCES users(stripe_id),
+    groupd_id INTEGER REFERENCES users(id)
 );
 
 CREATE TABLE paymentsOut (
     ID SERIAL PRIMARY KEY,
     payment_id VARCHAR UNIQUE,
     amount INTEGER,
-    user_id VARCHAR REFERENCES users(id)
+    user_id VARCHAR REFERENCES users(stripe_id),
+    group_id INTEGER REFERENCES users(id)
 );
