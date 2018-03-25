5125dffbae77c3a5a1bc0e72861d9469146f5c2a
Rachel91649 <rachelnjackson@yahoo.com>
2018-03-25 12:05:29 -0400

    rendering groups dynamically

--- client/src/Components/Groups/Groups.js
+++ client/src/Components/Groups/Groups.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import { ListGroup, ListGroupItem, Grid, Row, Col, PageHeader } from 'react-bootstrap';
+import { ListGroup, ListGroupItem, Row, Col, PageHeader } from 'react-bootstrap';
 
 
 const avatarStyle = {
@@ -12,21 +11,21 @@ const avatarStyle = {
 class Groups extends Component {
   constructor(){
     super();
+    this.state = {
+      groups: []
+    }
   }
 
   getAllGroups = () => {
     fetch("/groups")
-      .then(res => res.json())
-      .then((groups) => {
-        console.log(groups)
+    .then(res => res.json())
+    .then((groups) => {
+      console.log(groups.data[0]);
+      let data = groups.data
+      this.setState({
+        groups: data
       });
-    // axios.get("/groups")
-    // .then(response => {
-    //   console.log(response);
-    // })
-    // .catch(err => {
-    //   console.log("there was an error getting all groups");
-    // });
+    });
   }
 
   componentDidMount(){
@@ -34,12 +33,31 @@ class Groups extends Component {
   }
 
   render(){
+    const { groups } = this.state;
+    console.log("this is groups from state", groups);
     return(
       <div>
         Nav Bar will be above page header
         <PageHeader bsClass="groups-header">
           Example page header <small>Subtext for header</small>
         </PageHeader>
+        <ListGroup bsClass="groups-list-group">
+          {groups.map((group) => 
+            <ListGroupItem header={group.description} href="#"> 
+              <Row>
+                <Col md={4}>
+                  <p>{group.frequency} pay-in of <strong>$ {group.total_amount}</strong></p>
+                </Col>
+                <Col md={4}>
+                  <p>Savings Goal: <strong>$ {group.payout}</strong></p>
+                </Col>
+                <Col md={4}>
+                  
+                </Col>
+              </Row>
+            </ListGroupItem>
+          )}
+        </ListGroup>
         
           
         {/* <Grid> */}
@@ -53,35 +71,17 @@ class Groups extends Component {
                   <p>Group description (cuts off after x-amount of characters)</p>
                 </Col>
                 <Col md={4}>
-                  <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
-                  <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
-                  <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
-                  <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
-                  <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
-                  <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
-                </Col>
-              </Row>
-            </ListGroupItem>
-            <ListGroupItem header="Rainy Day Fund" href="#">
-            <Row>
-                <Col sm={4}>
-                  <p>Savings Goal: $500</p>
-                </Col>
-                <Col sm={4}>
-                  <p>Group description (cuts off after x-amount of characters)</p>
-                </Col>
-                <Col sm={4}>
-                  <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
-                  <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
-                  <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
-                  <img style={avatarStyle} alt="username" src="https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-6.jpg"/>
-                  <img style={avatarStyle} src="https://image.freepik.com/icones-gratis/perfil-simbolo-de-informacao_318-41154.jpg"/>
-                  <img style={avatarStyle} src="https://image.freepik.com/icones-gratis/perfil-simbolo-de-informacao_318-41154.jpg"/>
+                  <img style={avatarStyle} alt="username" src="https://image.flaticon.com/icons/svg/12/12324.svg"/>
+                  <img style={avatarStyle} alt="username" src="https://image.flaticon.com/icons/svg/12/12324.svg"/>
+                  <img style={avatarStyle} alt="username" src="https://image.flaticon.com/icons/svg/12/12324.svg"/>
+                  <img style={avatarStyle} alt="username" src="https://image.flaticon.com/icons/svg/12/12324.svg"/>
+                  <img style={avatarStyle} alt="username" src="https://image.flaticon.com/icons/svg/12/12324.svg"/>
+                  <img style={avatarStyle} alt="username" src="https://image.flaticon.com/icons/svg/12/12324.svg"/>
+                  <img style={avatarStyle} alt="username" src="https://image.flaticon.com/icons/svg/12/12324.svg"/>
                 </Col>
               </Row>
             </ListGroupItem>
           </ListGroup>
-        {/* </Grid> */}
           
       </div>
     )
