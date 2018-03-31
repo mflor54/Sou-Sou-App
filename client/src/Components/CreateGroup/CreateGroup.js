9fd7e50210e96dd91a52a8e24ba875946cf7e680
Rachel91649 <rachelnjackson@yahoo.com>
2018-03-31 17:28:01 -0400

    created CreateGroup folder, touched CreateGroup.js added, imported react and react router props, started basic page skeleton. touched CreateGroup.css

---
+++ client/src/Components/CreateGroup/CreateGroup.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+
+
+import './CreateGroup.css';
+
+
+class CreateGroup extends Component {
+  constructor(){
+    super()
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Create Group Page</h1>
+      </div>
+    )
+  }
+}
+
+
+export default CreateGroup;
+
