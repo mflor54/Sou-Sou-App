ae6f13e6ec39ee3363e9c1e3933e9181a9719fad
Rachel91649 <rachelnjackson@yahoo.com>
2018-03-31 19:03:11 -0400

    started skeleton of form

--- client/src/Components/CreateGroup/CreateGroup.js
+++ client/src/Components/CreateGroup/CreateGroup.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
+import {ProgressBar, Tabs, Tab, Popover, Form, FormGroup, ControlLabel, FormControl, Checkbox, Radio, Button, InputGroup, Input} from 'react-bootstrap';
 
 
 import './CreateGroup.css';
@@ -9,11 +10,20 @@ class CreateGroup extends Component {
   constructor(){
     super()
   }
-
+  //progress to show progress - animated
+  //tabs to navigate
+  //needs to redirect to the newly created group-profile page
   render(){
     return(
       <div>
         <h1>Create Group Page</h1>
+        <Tabs defaultActiveKey={1}>
+          <Tab eventKey={1} title="Group Name and Description">
+            <FormGroup>
+              
+            </FormGroup>
+          </Tab>
+        </Tabs>
       </div>
     )
   }
