897450342326fa0b034813605e4ea089215da8a4
Rachel91649 <rachelnjackson@yahoo.com>
2018-03-31 22:05:48 -0400

    group creation form skeleton complete

--- client/src/Components/CreateGroup/CreateGroup.js
+++ client/src/Components/CreateGroup/CreateGroup.js
@@ -9,7 +9,14 @@ import './CreateGroup.css';
 
 class CreateGroup extends Component {
   constructor(){
-    super()
+    super();
+    this.state = {
+      groupName: '',
+      groupDescription: '', 
+      payout: '', 
+      frequency: '', 
+      groupMembers: ''
+    }
   }
   //progress to show progress - animated
   //tabs to navigate
@@ -23,19 +30,62 @@ class CreateGroup extends Component {
             <TabContent>
               <Panel className="create-panel">
                 <Panel.Heading>
-                  <Panel.Title componentClass="h3"> Tab 1 content</Panel.Title>
+                  <Panel.Title componentClass="h3">Group Info</Panel.Title>
                 </Panel.Heading>
                 <Panel.Body> 
-                  Tab 1 content
+                  <ControlLabel>Group Name</ControlLabel>
+                  <FormControl 
+                    type="text"
+                    value="this.state.value"
+                    placeholder="Enter Group Name"
+                  />
+                  <ControlLabel>Group Description</ControlLabel>
+                  <FormControl
+                    componentClass="textarea" 
+                    type="text"
+                    value="this.state.value"
+                    placeholder="Enter Group Description"
+                  />
                 </Panel.Body>
               </Panel>
             </TabContent>
           </Tab>
           <Tab eventKey={2} title="Group Details">
-            Tab 2 content
+          <TabContent>
+            <Panel className="create-panel">
+              <Panel.Heading>
+                <Panel.Title componentClass="h3">Savings Goals </Panel.Title>
+              </Panel.Heading>
+              <Panel.Body> 
+                <ControlLabel>Payout Amount</ControlLabel>
+                  <FormGroup>
+                    <Radio name="100" inline>$100</Radio> 
+                    <Radio name="250" inline>$250</Radio>  
+                    <Radio name="1000" inline>$1000</Radio> 
+                  </FormGroup> 
+                <ControlLabel>Number of Group Members</ControlLabel>
+                  <FormGroup>
+                    <Radio name="3" inline>3</Radio> 
+                    <Radio name="5" inline>5</Radio>  
+                    <Radio name="9" inline>9</Radio> 
+                  </FormGroup>    
+                <ControlLabel>Payout Frequency</ControlLabel>
+                  <FormGroup>
+                    <Radio name="weekly" inline>Weekly</Radio> 
+                    <Radio name="bi-weekly" inline>Bi-Weekly</Radio>  
+                    <Radio name="monthly" inline>Monthly</Radio> 
+                  </FormGroup>                     
+                </Panel.Body>
+              </Panel>
+            </TabContent>
           </Tab>
           <Tab eventKey={3} title="Submit">
-            Tab 3 content
+            <TabContent>
+              <ControlLabel>Review Group Creation</ControlLabel>
+                <FormGroup>
+                  <Button>Submit</Button>
+                </FormGroup>
+            </TabContent>
           </Tab>
         </Tabs>
       </div>
